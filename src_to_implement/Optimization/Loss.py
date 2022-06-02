2a5d700672d6535c486cbec2590e18909b052d62
Chitra23Ahuja <105649388+Chitra23Ahuja@users.noreply.github.com>
2022-06-02 22:35:03 +0200

    Update Loss.py

--- src_to_implement/Optimization/Loss.py
+++ src_to_implement/Optimization/Loss.py
@@ -1,6 +1,5 @@
 import numpy as np
 
-
 class CrossEntropyLoss:
 
     def __init__(self):
@@ -8,16 +7,17 @@ class CrossEntropyLoss:
         self.temp_x = None
         self.eps = None
         self.temp_y = None
-        self.d = None
+        self.prediction_array = None
 
     def forward(self, prediction_tensor, label_tensor):
         self.eps = np.finfo(float).eps
-        self.d = np.array(prediction_tensor)
+        self.prediction_array = np.array(prediction_tensor)
         self.temp_x = np.log(np.array(prediction_tensor) + self.eps)
         self.temp_y = np.array(label_tensor)
         self.output = np.where(self.temp_y == 1, -self.temp_x, 0).sum()
         return self.output
 
     def backward(self, label_tensor):
-        error_tensor = np.where(label_tensor==1,-1/self.d, 0)
+        error_tensor = np.where(label_tensor == 1, -1 / self.prediction_array, 0)
         return error_tensor
+
