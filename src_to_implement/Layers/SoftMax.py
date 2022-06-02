45a41b12c4cf27b175da055ce93c1aa9f646e749
Chitra23Ahuja <105649388+Chitra23Ahuja@users.noreply.github.com>
2022-06-02 22:32:57 +0200

    Update SoftMax.py

--- src_to_implement/Layers/SoftMax.py
+++ src_to_implement/Layers/SoftMax.py
@@ -1,6 +1,4 @@
 import numpy as np
-from .Base import BaseLayer
-
 
 class SoftMax:
     def __init__(self):
@@ -8,13 +6,15 @@ class SoftMax:
         self.trainable = False
         self.output = None
         self.softmax_gradient = None
+
     def forward(self, input_tensor):
-        exps = np.exp(input_tensor-np.max(input_tensor))
+        exps = np.exp(input_tensor - np.max(input_tensor))
         self.output = exps / exps.sum(axis=1, keepdims=True)
         self.softmax_gradient = self.output.copy()
-        print(self.softmax_gradient)
         return self.output
 
     def backward(self, error_tensor):
-        self.output= self.softmax_gradient * (error_tensor- (error_tensor * self.softmax_gradient).sum(axis=1)[:, None])
+        self.output = self.softmax_gradient * (
+                    error_tensor - (error_tensor * self.softmax_gradient).sum(axis=1)[:, None])
         return self.output
+
