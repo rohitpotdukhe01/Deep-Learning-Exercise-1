94e17bba1ae70339fc4f1b14c54b311b8978ce36
Chitra23Ahuja <105649388+Chitra23Ahuja@users.noreply.github.com>
2022-06-02 16:35:06 +0200

    Update ReLU.py

--- src_to_implement/Layers/ReLU.py
+++ src_to_implement/Layers/ReLU.py
@@ -1,16 +1,21 @@
-class ReLU:
+import numpy as np
+from .Base import BaseLayer
+
+
+class ReLU(BaseLayer):
+
     def __init__(self):
+        super().__init__()
+        self.trainable = False
         self.output = None
+        self.relu_gradient = None
 
     def forward(self, input_tensor):
-
-        for input in input_tensor:
-            if input > 0:
-                self.output.append(input)
-            else:
-                self.output.append(0)
+        self.output=np.clip(input_tensor, a_min=0, a_max=None)
+        self.relu_gradient=self.output.copy()
         return self.output
 
     def backward(self, error_tensor):
-
-        pass
+        self.relu_gradient[self.relu_gradient > 0] = 1
+        self.output = np.multiply(error_tensor, self.relu_gradient)
+        return self.output
